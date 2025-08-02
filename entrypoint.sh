18d053531834e055f5785ed490b434b244a2f4d3
Nils Wenning <nilswe@Mac.lan>
2025-08-02 16:14:47 +0200

    add Fix-Mode patch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# Check if the volume is mounted and not empty
+
 if [ -z "$(ls -A /uhrh)" ]; then
-  echo "Volume is empty, copying files from backup..."
-  cp -r /uhrh_backup/* /uhrh/
+	echo "Volume is empty, copying files from backup..."
+	cp -r /uhrh_backup/* /uhrh/
+  patch /uhrh/UHRR < /setmode-fix.patch
 fi
 
-exec "$@"
+exec /uhrh/UHRR
